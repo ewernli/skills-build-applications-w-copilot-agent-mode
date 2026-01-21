cdc13d6f79938add0f7297b621e0fbbc79e4cc4f
ewernli <erwann.wernli@gmail.com>
2026-01-21 15:40:22 +0000

    Start server

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -1,3 +1,5 @@
+# Utiliser le modèle User personnalisé
+AUTH_USER_MODEL = 'octofit_tracker.User'
 """
 Django settings for octofit_tracker project.
 
@@ -25,7 +27,11 @@ SECRET_KEY = 'django-insecure-#q+&i%9il053@6s__p5!1raf#tiis=rjns4jj*_8*s+^(m^qn1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
